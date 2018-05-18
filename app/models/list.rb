422abaab11bf8a95aa7d4c5e711a95a100bde0bc
StephenWeil <stephen.weil@gmail.com>
2018-05-18 00:01:11 -0500

    Add additional validations for Task and List models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,17 @@
 class List < ApplicationRecord
+    validates :title, length: { in: 1..100, message: 'must be between 1 and 100 characters.' }
+    validates :description, length: { in: 1..1000, message: 'must be between 1 and 1000 characters.' }
+
+    has_many :tasks
+    has_many :completed_tasks, -> { where(status: 'completed') }, class_name: 'Task'
+    has_many :claimed_tasks, -> { where(status: 'claimed') }, class_name: 'Task'
+    has_many :unclaimed_tasks, -> { where(status: 'unclaimed') }, class_name: 'Task'
+
+    has_many :list_users
+    has_many :created_list_users, -> { where(creator: true) }, class_name: 'ListUser'
+    has_many :followed_list_users, -> { where(creator: false) }, class_name: 'ListUser'
+
+    has_many :users, through: :list_users
+    has_many :created_users, through: :created_list_users, source: :user
+    has_many :followed_users, through: :followed_list_users, source: :user
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,43 @@
 class Task < ApplicationRecord
+    validates :title, length: { in: 1..50, message: 'must be between 1 and 50 characters.' }
+    validates :description, length: { in: 1..150, message: 'must be between 1 and 150 characters.' }
+    validates :status_in_valid_list
+    validates :due_at_in_future, on: :create
+
+    belongs_to :list
+
+    has_many :task_users
+    has_many :completed_task_users, -> { where(completed: true) }, class_name: 'TaskUser'
+    has_many :claimed_task_users, -> { where(completed: false) }, class_name: 'TaskUser'
+    
+    has_many :users, through: :task_users
+    has_many :completed_users, through: :completed_task_users, source: :user
+    has_many :claimed_users, through: :claimed_task_users, source: :user
+
+    before_create :set_defaults
+    before_save :convert_times_to_utc
+
+    private
+
+    def set_defaults
+      self.status = 'unclaimed'
+      self.completed = false
+    end
+
+    def convert_times_to_utc
+    end
+
+    # validation methods
+    
+    def status_in_valid_list
+      errors.add(:status, "must be in: #{valid_stauses.join(', ')}") unless valid_statuses.include?(self.status)
+    end
+
+    def valid_stauses
+      %w[completed claimed unclaimed]
+    end
+
+    def due_at_in_future
+      errors.add(:due_at, 'must be in the future.') unless self.due_at.to_utc > Time.zone.now
+    end
 end
