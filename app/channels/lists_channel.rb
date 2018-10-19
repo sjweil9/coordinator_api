57e61a3c9bf2bab6b865bbbe4dbdc21aabf84e4b
Stephen Weil <stephen.weil@gmail.com>
2018-10-18 21:21:26 -0500

    Handle disconnect/invalid List

--- app/channels/lists_channel.rb
+++ app/channels/lists_channel.rb
@@ -1,11 +1,17 @@
 class ListsChannel < ApplicationCable::Channel
   def subscribed
     puts 'got a subscriber!'
-    list = List.find(params[:list_id])
+    reject unless list
     stream_for list
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
+  end
+
+  private
+
+  def list
+    @list ||= List.find(params[:list_id])
   end
 end
